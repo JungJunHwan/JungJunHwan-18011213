dd84f3b5639ba3c0cdb8a8ee3895bfbbc9a6bfe3
user <user@JJH-PC>
2023-05-25 19:20:15 +0900

    used lesson4 trajectory eq. (6 page) & lesson7 (11 page)

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -1,1 +1,19 @@
 function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
+
+% % Solve r and v in perifocal frame from orbit parameters
+% % - function 명 : solveRangeInPerifocalFrame, solveVelocityInPerifocalFrame 
+% % - input : semimajor_axis, eccentricity, true_anomaly  : 3개 scalar 변수
+% % - output : rangeInPQW, velocityInPQW (unit : km/s) 3-by-1 matrix each, in perifocal coordinate
+% % μ = 3.986004418× 1014 [m3 s−2]
+% % ex)
+% % rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
+% % velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
+% % rangeInPQW = [10000 10000 0]T ;     velocityInPQW = [-100 100 0]T; 
+% % 
+% % 주의사항
+% % 1) 생성된 Repository에 matlab function 생성 후 commit - push
+% % 2) Input & output 각도 unit : deg, 거리 unit : km로 통일
+
+p = semimajor_axis*(1 - eccentricity^2);
+r = p / (1 + eccentricity*cos(true_anomaly));
+rangeInPQW = [r*cos(true_anomaly); r*sin(true_anomaly); 0];

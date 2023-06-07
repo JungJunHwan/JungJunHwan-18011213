290e9b81a3e05ca124da36da7ad64212d3a451e7
user <user@JJH-PC>
2023-06-07 16:14:29 +0900

    done

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -3,3 +3,8 @@ function DCM = ECI2ECEF_DCM(time)
 % - input : time ([YYYY,MM,DD,hh,mm,ss] format)
 % - output : DCM matrix (3-by-3)
 
+theta_g = deg2rad(siderealTime(juliandate(time)));
+Cos = cos(theta_g);
+Sin = sin(theta_g);
+
+DCM = [Cos Sin 0;-Sin Cos 0;0 0 1];

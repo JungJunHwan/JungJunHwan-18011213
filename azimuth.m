8cb0c8a8e2826321ab80df52f31ddf4524595e80
user <user@JJH-PC>
2023-06-07 16:17:00 +0900

    start

---
+++ azimuth.m
@@ -0,0 +1,5 @@
+function az = azimuth(ENU)
+% - function 명 : az = azimuth(ENU)
+% - input : ENU (행이 시각 또는 위성수, 열이 ENU로 구성된 위성 ENU 위치로 구성된 n-by-3 matrix, 단위 km)
+% - output : azimuth angle (1-by-n, 단위 deg)
+

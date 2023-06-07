6413f0bf8993ae71eab602bc2819877be0dbf437
user <user@JJH-PC>
2023-06-07 16:23:55 +0900

    done

--- azimuth.m
+++ azimuth.m
@@ -3,3 +3,7 @@ function az = azimuth(ENU)
 % - input : ENU (행이 시각 또는 위성수, 열이 ENU로 구성된 위성 ENU 위치로 구성된 n-by-3 matrix, 단위 km)
 % - output : azimuth angle (1-by-n, 단위 deg)
 
+E = ENU(:,1);
+N = ENU(:,2);
+
+az = acos(N/sqrt(E.^2 + N.^2));
